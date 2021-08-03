5bed3504e68b42cc8e7f681a10dbfc72a5f5d555
Andrej-oss <snowtomsawyer@gmail.com>
2021-08-03 22:06:23 +0200

    - aws & virtualisation added

--- book-records-task-angular/src/app/modules/router-module.ts
+++ book-records-task-angular/src/app/modules/router-module.ts
@@ -1,7 +1,6 @@
 import {Routes} from "@angular/router";
 import {HomeComponent} from "../component/home/home.component";
 import {FormComponent} from "../component/form/form.component";
-import {AppComponent} from "../component/app-page/app.component";
 
 export const routes: Routes = [
     {path: 'book', component: HomeComponent},
